17ca5c67f4530c1c48a690dac26fed1a5b1650b7
richard-ma <richard.ma.19850509@gmail.com>
2022-09-07 11:43:17 +0800

    add mpg123 player

---
+++ player.py
@@ -0,0 +1,77 @@
+from abc import ABC, abstractmethod
+import subprocess
+
+
+class Player(ABC):
+    @abstractmethod
+    def play(self, track_name):
+        pass
+
+    @abstractmethod
+    def pause(self):
+        pass
+
+    @abstractmethod
+    def stop(self):
+        pass
+
+    @abstractmethod
+    def mute(self, op=True):
+        pass
+
+    @abstractmethod
+    def volume(self, percent):
+        pass
+
+
+class mpg123_Player(Player):
+    def __init__(self):
+        self.p = None
+        self._volume = 50  # 50% volume
+
+    def __write(self, cmd):
+        cmd += '\n'
+
+        try:
+            self.p.stdin.write(cmd.encode())
+            self.p.stdin.flush()
+        except Exception as err:
+            raise Exception("{0}: {1}".format(type(err).__name__, err))
+
+    def run(self):
+        self.p = subprocess.Popen(['mpg123', '-R'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, shell=False)
+        print("Start Player")
+
+    def play(self, track_name):
+        print("Play: ", track_name)
+        if self.p is not None:
+            self.__write(f'LOAD {track_name}'.format(track_name=track_name))
+            return True
+        else:
+            return False
+
+    def pause(self):
+        if self.p is not None:
+            self.__write('PAUSE')
+
+    def stop(self):
+        if self.p is not None:
+            self.__write('STOP')
+
+    def mute(self, mute=True):
+        if self.p is not None:
+            if mute:
+                self.__write('MUTE')
+            else:
+                self.__write('UNMUTE')
+
+    def volume(self, percent):
+        if self.p is not None:
+            self.__write(f'VOLUME {percent}'.format(percent=percent))
+
+
+if __name__ == "__main__":
+    track_name = '/home/share/Music/周杰伦-听见下雨的声音.mp3'
+    player = mpg123_Player()
+    player.run()
+    player.play(track_name)
